7163f5b7c40bff68cc5e758dbce5766f60822e14
unknown <michael.n.olaya@gmail.com>
2024-07-29 18:39:10 -0400

    wip hw2

--- homeworks/hw2/hw2.py
+++ homeworks/hw2/hw2.py
@@ -1,87 +1,131 @@
+import typing
+import itertools
+
 import numpy as np
 import matplotlib.pyplot as plt
 from attrs import define
 
+# Compute the value of a shape function for a series of grid points in the natural coordinate system.
+def _compute_shape_func(shape_func: typing.Callable, eta_grid: tuple[np.ndarray]):
+    val = []
+    for i in range(eta_grid[0].shape[0]):
+        row = []
+        for j in range(eta_grid[1].shape[0]):
+            e1, e2 = eta_grid[0][i, j], eta_grid[1][i][j]
+            row.append(shape_func(e1, e2))
+        val.append(np.array(row))
+    return np.array(val)
+
 @define
 class Node:
-    x: float
-    y: float
-    global_num: int
-    elem_num: int
+    local_num: int
+    local_coords: np.ndarray
+    natural_coords: np.ndarray
 
 @define
 class Linear2D:
-    # nodes: list[Node]
+    nodes: list[Node]
+
+    @classmethod
+    def from_element_coords(cls, coords: list[np.ndarray]):
+        nodes = [
+            Node(local_num=1, local_coords=coords[0], natural_coords=np.array([-1, -1])),
+            Node(local_num=2, local_coords=coords[1], natural_coords=np.array([1, -1])),
+            Node(local_num=3, local_coords=coords[2], natural_coords=np.array([1, 1])),
+            Node(local_num=4, local_coords=coords[3], natural_coords=np.array([-1, 1])),
+        ]
+        return cls(nodes)
+
+    def N(self, eta_1: float, eta_2: float) -> np.ndarray:
+        return [
+            [self.shape_n1(eta_1, eta_2), 0, self.shape_n2(eta_1, eta_2), 0, self.shape_n3(eta_1, eta_2), 0, self.shape_n4(eta_1, eta_2), 0],
+            [0, self.shape_n1(eta_1, eta_2), 0, self.shape_n2(eta_1, eta_2), 0, self.shape_n3(eta_1, eta_2), 0, self.shape_n4(eta_1, eta_2)],
+        ]
+
+    @property
+    def x_natural(self) -> np.ndarray:
+        return list(itertools.chain.from_iterable([node.natural_coords for node in self.nodes]))
+
+    @property
+    def x_element(self) -> np.ndarray:
+        return list(itertools.chain.from_iterable([node.local_coords for node in self.nodes]))
     
-    def shape_n1(self, eta_1: float, eta_2: float) -> float:
+    def get_shape_funcs(self) -> list:
+        return [self.shape_n1, self.shape_n2, self.shape_n3, self.shape_n4]
+    
+    @staticmethod
+    def shape_n1(eta_1: float, eta_2: float) -> float:
         return 0.25*(eta_1 - 1)*(eta_2 - 1)
 
-    def shape_n2(self, eta_1: float, eta_2: float) -> float:
+    @staticmethod
+    def shape_n2(eta_1: float, eta_2: float) -> float:
         return -0.25*(eta_1 + 1)*(eta_2 - 1)
 
-    def shape_n3(self, eta_1: float, eta_2: float) -> float:
+    @staticmethod
+    def shape_n3(eta_1: float, eta_2: float) -> float:
         return 0.25*(eta_1 + 1)*(eta_2 + 1)
 
-    def shape_n4(self, eta_1: float, eta_2: float) -> float:
+    @staticmethod
+    def shape_n4(eta_1: float, eta_2: float) -> float:
         return -0.25*(eta_1 - 1)*(eta_2 + 1)
     
-    def plot_shape_functions(self, nvals: int = 1000) -> None:
+    def interpolate(self, nodal_vec: np.ndarray, nvals: int = 100) -> tuple[np.ndarray]:
         # Create grid for the shape functions
         eta_1 = np.linspace(-1, 1, nvals)
         eta_2 = np.linspace(-1, 1, nvals)
         eta_1, eta_2 = np.meshgrid(eta_1, eta_2)
 
+        # Interpolate for each point on the grid
+        interpolated = []
+        for i in range(nvals):
+            row = []
+            for j in range(nvals):
+                e1, e2 = eta_1[i, j], eta_2[i, j]
+                row.append(np.matmul(self.N(e1, e2), nodal_vec))
+            interpolated.append(np.array(row))
+        interpolated = np.array(interpolated)
+        return interpolated[:, :, 0], interpolated[:, :, 1]  
+
+    def plot_shape_functions(self, domain: str = 'natural', nvals: int = 100, nlevels: int | None = None) -> None:
+        # Get element coordinates in natural or element coordinate system
+        nodal_vec = element.x_natural
+        labels = {'x': r'$\eta_{1}$', 'y': r'$\eta_{2}$'}
+        if domain.lower() == 'element': 
+            nodal_vec = element.x_element
+            labels.update({'x': r'$x_{1}$', 'y': r'$x_{2}$'})
+        coords_x, coords_y = element.interpolate(nodal_vec=nodal_vec, nvals=nvals)
+
+        # Create grid for the shape functions
+        eta_1 = np.linspace(-1, 1, nvals)
+        eta_2 = np.linspace(-1, 1, nvals)
+        grid = np.meshgrid(eta_1, eta_2)
+
         # Create figure and axes
-        # fig, axes = plt.subplots(ncols=2, nrows=2)
         fig, axes = plt.subplots(ncols=2, nrows=2, figsize=(10, 10))
 
         # Unravel axes into single list
         axes = [ax for _axes in axes for ax in _axes]
 
         # Loop through shape functions and plot contours
+        plt_kw = {}
         shape_funcs = {1: self.shape_n1, 2: self.shape_n2, 3: self.shape_n3, 4: self.shape_n4}
         for ax, (i, func) in zip(axes, shape_funcs.items()):
-            ax.hlines(0, -1, 1, color='tab:grey', lw=0.5, linestyle='--')
-            ax.vlines(0, -1, 1, color='tab:grey', lw=0.5, linestyle='--')
-            c = ax.contourf(eta_1, eta_2, func(eta_1, eta_2))
+            val = _compute_shape_func(func, grid)
+            if nlevels is not None: plt_kw.update({'levels': np.linspace(np.min(val), np.max(val), nlevels)})
+            c = ax.contourf(coords_x, coords_y, val, **plt_kw)
             ax.set_title(rf'Shape function $N_{{{i}}}$')
-            ax.set_xlabel(r'$\eta_{1}$ position')
-            ax.set_xlim(-1, 1)
-            ax.set_ylabel(r'$\eta_{2}$ position')
-            ax.set_ylim(-1, 1)
-            ax.set_aspect('equal')
+            cbar = plt.colorbar(c, ticks=[0, 0.25, 0.5, 0.75, 1.0])
+            ax.set_xlabel(labels['x'])
+            ax.set_ylabel(labels['y'])
         return fig, axes
 
-    
-element = Linear2D()
-fig, axes = element.plot_shape_functions()
-
-# fig, axes = plt.subplots(ncols=2, nrows=2, figsize=(10, 10))
-# axes = [ax for _axes in axes for ax in _axes]
-# for ax in axes:
-#     ax.hlines(0, -1, 1, color='tab:grey', lw=0.5, linestyle='--')
-#     ax.vlines(0, -1, 1, color='tab:grey', lw=0.5, linestyle='--')
-
-# c = axes[0].contourf(eta_1, eta_2, element.shape_n1(eta_1, eta_2))
-# axes[0].set_title(r'$N_{1}$')
-# axes[0].set_xlabel(r'$\eta_{1}$ position')
-# axes[0].set_ylabel(r'$\eta_{2}$ position')
-# c = axes[1].contourf(eta_1, eta_2, element.shape_n2(eta_1, eta_2))
-# axes[1].set_title(r'$N_{2}$')
-# c = axes[2].contourf(eta_1, eta_2, element.shape_n3(eta_1, eta_2))
-# axes[2].set_title(r'$N_{3}$')
-# c = axes[3].contourf(eta_1, eta_2, element.shape_n4(eta_1, eta_2))
-# axes[3].set_title(r'$N_{4}$')
-# # fig.colorbar(c, ax=axes[2], fraction=0.02, pad=0.1)
+nvals = 100
+element = Linear2D.from_element_coords(
+    [np.array([0, 0]), np.array([12, -1]), np.array([15, 8]), np.array([-1, 10])]
+)
+fig, axes = element.plot_shape_functions(domain='natural', nvals=nvals, nlevels=50)
 fig.tight_layout()
-# ax.set_aspect('equal')
-# plt.show()
 
-nodes = np.array([[0, 0], [12, -1], [15, 8], [-1, 10]])
-x = np.array([
-    element.shape_n1(nodes[0, 0], nodes[0, 1]),
-    element.shape_n2(nodes[1, 0], nodes[1, 1]),
-    element.shape_n3(nodes[2, 0], nodes[2, 1]),
-    element.shape_n4(nodes[3, 0], nodes[3, 1]),
-])
-print(x)
+fig, axes = element.plot_shape_functions(domain='element', nvals=nvals, nlevels=50)
+fig.tight_layout()
+plt.show()
